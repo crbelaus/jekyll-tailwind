56028aab93cc8fad8474fc9c8bcb461d75312f31
crbelaus <cristian@crbelaus.com>
2024-03-12 19:15:56 +0100

    Remove comments

--- lib/jekyll-tailwind.rb
+++ lib/jekyll-tailwind.rb
@@ -1,22 +1,10 @@
 # frozen_string_literal: true
 
-# require_relative "jekyll-tailwind/version"
+require_relative "jekyll-tailwind/version"
 require_relative "jekyll-tailwind/installer"
 
 require "jekyll"
 require "open3"
-
-# module Jekyll
-#   class Tailwind
-#     def install(target)
-#       # TODO configure the version
-#       version = "3.4.1"
-#       url = URI"https://github.com/tailwindlabs/tailwindcss/releases/download/v#{version}/tailwindcss-#{target}"
-
-
-#     end
-#   end
-# end
 
 Jekyll::Hooks.register [:site], :post_write do |site|
   Jekyll::Tailwind::Installer.install_and_run
