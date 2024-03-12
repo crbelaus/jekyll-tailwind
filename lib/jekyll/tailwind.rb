ae038bef59257fbc0e3008059b91e1769f398faf
crbelaus <cristian@crbelaus.com>
2024-03-12 16:31:46 +0100

    Initial commit

---
+++ lib/jekyll/tailwind.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tailwind/version"
+
+module Jekyll
+  module Tailwind
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/tailwind/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Tailwind
+    VERSION = "0.1.0"
+  end
+end
