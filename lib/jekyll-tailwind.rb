bdc6208730c1c45efad1b7be6374c8f49642fc32
Stanislav (Stas) Katkov <github@skatkov.com>
2024-09-27 23:07:09 +0200

    Integrating tailwindcss-ruby

--- lib/jekyll-tailwind.rb
+++ lib/jekyll-tailwind.rb
@@ -5,10 +5,56 @@ require_relative "jekyll-tailwind/installer"
 
 require "jekyll"
 
+# frozen_string_literal: true
+
+require_relative "jekyll-tailwind-cli/version"
+
+require "jekyll"
+require "tailwindcss/ruby"
+
+module Jekyll
+  class Tailwind
+
+    def intialize(config)
+      @config = config.fetch("config_path", "tailwind.config.js")
+      @postcss = "postcss.config.js"
+      @inputs = array_wrap(config["input"])
+      @output = config.fetch("output", "_site/assets/css/app.css")
+    end
+
+    def compile
+      command = [
+                Tailwindcss::Ruby.executable,
+                "--output", @output,
+                "--config", @config,
+              ]
+
+      @inputs.each do |input|
+        # There could be multiple input files or non at all.
+        command += ["--input", input]
+      end
+
+      command += ["--postcss", @postcss] if File.exist?(@postcss)
+
+      `#{command.join(' ')}`
+    end
+
+    private
+
+    # Taken from rails/activesupport/lib/active_support/core_ext/array/wrap.rb
+    def array_wrap(object)
+      if object.nil?
+        []
+      elsif object.respond_to?(:to_ary)
+        object.to_ary || [object]
+      else
+        [object]
+      end
+    end
+  end
+end
+
+# It's important to run TailwindCSS compilation after Jekyll has finished with build steps. TailwindCSS will analyse resulting HTML and produce a CSS file with attributes that are actually in use, this is done to produce a smaller CSS file.
 Jekyll::Hooks.register [:site], :post_write do |site|
-  tailwind = Jekyll::Tailwind::Installer.new(
-    version: site.config.dig("tailwind", "version"),
-    config_path: site.config.dig("tailwind", "config_path")
-  )
-  tailwind.install_and_run
+  Jekyll::Tailwind.new(site.config.fetch('tailwind', {})).compile
 end
